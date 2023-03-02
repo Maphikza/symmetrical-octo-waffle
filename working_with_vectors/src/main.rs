ca60091c00eda122b4f28da4654de285ccb96f47
Maphikza <stapisi@protonmail.com>
2023-03-02 10:40:19 +0200

    Adding print statements to make sense of what the code is doing

--- working_with_vectors/src/main.rs
+++ working_with_vectors/src/main.rs
@@ -14,24 +14,36 @@ through millions of pages?";
     for (i, line) in haystack.lines().enumerate() { //3.Iterate through the lines, recording line numbers where matches are encountered
         if line.contains(needle) {
             tags.push(i);
+            println!("i is:{}", i);
+            println!("tags is {:?}", tags);
             let v = Vec::with_capacity(2 * context_lines + 1); //4-5. Vec::with_capacity(n) reserves space for n items - No explicit type signature is required here, as it can be inferred via the definition of ctx on line 15.
+            println!("{:?}", v.capacity());
+            println!("ctx before: {:?}", ctx);
             ctx.push(v);
+            println!("added capacity for ctx vec is: {:?}", ctx.capacity());
         }
     }
+    println!("ctx after: {:?}", ctx);
     if tags.len() == 0 { //6.When there are no matches, exit early
         return;
     }
     for (i, line) in haystack.lines().enumerate() { //7.For each tag, at every line, check to see if we are nearby a match. When we are, add that line to the relevant Vec<T> within ctx.
         for (j, tag) in tags.iter().enumerate() {
             let lower_bound = tag.saturating_sub(context_lines); //8. usize.saturating_sub() is subtraction that returns 0 on integer underflow rather than crashing the program (CPUs don’t like attempting to send usize below zero)
+            // println!("lower bound {}", lower_bound.to_string());
             let upper_bound = tag + context_lines;
+            // println!("upper bound {}", upper_bound.to_string());
             if (i >= lower_bound) && (i <= upper_bound) {
                 let line_as_string = String::from(line); //9. Copy line into a new String and store that locally for each match
                 let local_ctx = (i, line_as_string);
+                println!("local ctx: {:?}", local_ctx);
                 ctx[j].push(local_ctx);
+                println!("ctx[j]: {:?}", ctx[j]);
             }
         }
     }
+    println!("ctx at the end: {:?}", ctx);
+    //This line unpacks all the items added to ctx and then iterates on them to print the line numbers in the expected form line by line.
     for local_ctx in ctx.iter() {
         for &(i, ref line) in local_ctx.iter() { //10. ref line informs the compiler that we wish to borrow this value, rather than move it. These two terms are explained fully later in later chapters.
             let line_num = i + 1;
